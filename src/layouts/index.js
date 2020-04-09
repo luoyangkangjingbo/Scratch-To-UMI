1281190993987aaa56a4f4eec195a43924eda8e1
Bilinear <Bilinear@succ.com>
2020-04-09 10:48:51 +0800

    Reinit Project

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,17 @@
 import styles from './index.css';
 
+// function BasicLayout(props) {
+//   return (
+//     <div className={styles.normal}>
+//       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+//       {props.children}
+//     </div>
+//   );
+// }
+
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+    <div>
       {props.children}
     </div>
   );
